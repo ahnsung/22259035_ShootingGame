c6f9d39c4d3f558d6508e18c96458350a8795ddf
ahnsung <81170096+ahnsung@users.noreply.github.com>
2026-03-25 14:07:47 +0900

    added_monster&&monstermove_scripts

---
+++ Assets/Scripts/MonsterManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+    public GameObject prefabMonster;
+
+    float nowTime;
+    float minTime = 1f;
+    float maxTime = 5f;
+    public float createTime = 1f;
+
+    private void Start()
+    {
+        createTime = Random.Range(minTime, maxTime);
+    }
+
+    private void Update()
+    {
+        nowTime += nowTime + Time.deltaTime;
+
+        if(nowTime > createTime)
+        {
+            GameObject monster = Instantiate(prefabMonster);
+            monster.transform.position = transform.position;
+            nowTime = 0; // 누적 시간 초기화
+        }
+    }
+}
